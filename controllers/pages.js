e6ec5ce8be83dec96899163939da11e9fa8e09a1
Sahirin <96673365+Matalang@users.noreply.github.com>
2022-10-09 13:03:04 +0700

    Add files via upload

---
+++ controllers/pages.js
@@ -0,0 +1,11 @@
+export const Home = (req, res) => {
+    res.send('Welcome to Homepage');
+};
+
+export const About = (req, res) => {
+    res.send('Ini adalah halaman About');
+};
+
+export const Contact = (req, res) => {
+    res.send('Ini dalah halaman Contact');
+};
